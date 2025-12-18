2b9222801b699e7039d2edbc4cdeb1c2cee57697
bala-amypo <bala@amypo.in>
2025-12-18 06:48:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,32 +1,39 @@
 package com.example.demo.entity;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Column;
 @Entity
-//@Table(name="students")
-
+// @Table(name = "students")
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+    @NotBlank(message="Name filed cannot be empty")
+    @Size(min=3,max=20,message="The user name must be min of 3 and max of 20 character")
     private String name;
+    @Email(message="Invalid email id")
+    @Column(unique=true)
     private String email;
-    
-    public Long getId(){
+
+    // @NotNull
+    // @Size(min,max)
+    // @Min
+    // @Max
+    // @Pattern(reg)
+    // @Postive
+
+    public Long getId() {
         return id;
-
     }
 
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
-    
     }
+
     public String getName() {
         return name;
     }
@@ -43,15 +50,14 @@ public class Student {
         this.email = email;
     }
 
-public Student(Long id, String name, String email) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
+    public Student(String name,String email) {
+        this.name = name;
+        this.email = email;
+    }
 
-}
+    public Student() {
+    }
+    
 
-public Student() {
-
-}
     
 }
---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.example.aiml.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+    
+}
